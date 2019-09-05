107e6affb81dce3ea270016a19aca8335261343b
Alex <abuendiae1500@alumno.ipn.mx>
2019-09-05 11:18:48 -0500

    Ingreso de datos de usuario.

---
+++ app/src/main/java/com/apptt/axdecor/activities/DatosUsuarioActivity.kt
@@ -0,0 +1,16 @@
+package com.apptt.axdecor.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.apptt.axdecor.R
+import kotlinx.android.synthetic.main.activity_datos_usuario.*
+
+class DatosUsuarioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_datos_usuario)
+    }
+}
---
+++ app/src/main/java/fragments/Datos1Fragment.kt
@@ -0,0 +1,39 @@
+package fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.navigation.findNavController
+
+import com.apptt.axdecor.R
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_datos1.*
+
+class Datos1Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_datos1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val nombre = etxtNombre as EditText
+        val edad = etxtEdad as EditText
+        btnSiguiente.setOnClickListener {
+            if (nombre.text.toString().equals("") || edad.text.toString().equals("") ) {
+                Snackbar.make(btnSiguiente, "Parece que olvidas algo.", Snackbar.LENGTH_SHORT)
+                    .show()
+            } else {
+                it.findNavController().navigate(Datos1FragmentDirections.actionDatos1FragmentToDatos2Fragment(nombre.text.toString(),edad.text.toString().toInt()))
+            }
+
+        }
+    }
+}
