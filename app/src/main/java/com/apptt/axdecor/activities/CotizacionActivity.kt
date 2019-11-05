4083d369158e85b6353e262e41fdc08edcee5451
AleEscom03 <abuendiae1500@alumno.ipn.mx>
2019-11-05 12:49:34 -0600

    Correccion diseño modelo

--- app/src/main/java/com/apptt/axdecor/activities/CotizacionActivity.kt
+++ app/src/main/java/com/apptt/axdecor/activities/CotizacionActivity.kt
@@ -3,9 +3,10 @@ package com.apptt.axdecor.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.apptt.axdecor.R
+import com.apptt.axdecor.db.Entities.ModelModel
 
-class CotizacionActivity : AppCompatActivity() {
-
+class CotizacionActivity() : AppCompatActivity() {
+    lateinit var listaModelos: ArrayList<ModelModel>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cotizacion)
