2efd98896b2f35a9f4cad7c1b9254bd098f83b22
AleEscom03 <abuendiae1500@alumno.ipn.mx>
2019-11-05 16:31:33 -0600

    Provedores seleccion

---
+++ app/src/main/java/com/apptt/axdecor/adapters/ProveedoresAdapter.kt
@@ -0,0 +1,37 @@
+package com.apptt.axdecor.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.recyclerview.widget.RecyclerView
+import com.apptt.axdecor.R
+import com.apptt.axdecor.domain.CategoryProvider
+
+class ProveedoresAdapter(context: Context, list: List<CategoryProvider>) :
+    RecyclerView.Adapter<ProveedoresAdapter.PlaceViewHolder>() {
+    val mContext = context
+    val mList = list
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceViewHolder {
+        return ProveedoresAdapter.PlaceViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.proveedor_radio_button,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    override fun onBindViewHolder(holder: PlaceViewHolder, position: Int) {
+
+    }
+
+    class PlaceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val radioBoton = itemView.findViewById<RadioButton>(R.id.rbProv)
+    }
+}
