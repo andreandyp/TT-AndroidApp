e2fcbed1ac6d7f1204a617014e2a5117e2115275
AleEscom03 <abuendiae1500@alumno.ipn.mx>
2019-11-06 18:16:23 -0600

    Nav drawer completo

---
+++ app/src/main/java/com/apptt/axdecor/activities/ModoDecoracionActivity.kt
@@ -0,0 +1,12 @@
+package com.apptt.axdecor.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModoDecoracionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modo_decoracion)
+    }
+}
---
+++ app/src/main/java/com/apptt/axdecor/activities/SelectEstiloActivity.kt
@@ -0,0 +1,12 @@
+package com.apptt.axdecor.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectEstiloActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_estilo)
+    }
+}
